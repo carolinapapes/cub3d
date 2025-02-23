cd19b46f4fbc872a8521e92c2fe8c09bb6b17ac1
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2025-02-23 16:23:32 +0100

    base for future free struct

---
+++ src/free_struct.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_struct.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/23 15:39:47 by kkoval            #+#    #+#             */
+/*   Updated: 2025/02/23 16:14:15 by kkoval           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+
+void	free_int_array(int ***array, int size_y)
+{
+	int	y;
+
+	y = 0;
+	if (!array || !*array)
+		return ;
+	while (y < size_y)
+	{
+		free((*array)[y]);
+		y++;
+	}
+	free(*array);
+	*array = NULL;
+	return ;
+}
+
+void	free_char_array(char ***split)
+{
+	int	i;
+
+	if (!split || !*split)
+		return ;
+	i = 0;
+	while ((*split)[i])
+	{
+		free((*split)[i]);
+		i++;
+	}
+	free(*split);
+	*split = NULL;
+	return ;
+}
+
+// free the whole struct start in case of error or exit
+void	free_start(t_start *start)
+{
+	if (!start)
+		return ;
+	if (start->map.map != NULL)
+		free_char_array(&start->map.map);
+	if (start->map.map_int != NULL)
+		free_int_array(&start->map.map_int, start->map.size_int.y);
+	return ;
+}
