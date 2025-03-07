3c48e39349ebc0ed144b1ff9eeeb42be9dcba1b0
Kate <108870914+LeelooDa11as@users.noreply.github.com>
2025-03-07 01:56:21 +0100

    limpieza de .d .o files

--- obj/file_handler.d
+++
@@ -1,5 +0,0 @@
-obj/file_handler.o: src/file_handler.c src/cube3d.h \
-  src/../lib/MLX42/include/MLX42/MLX42.h src/../lib/libft/libft.h
-src/cube3d.h:
-src/../lib/MLX42/include/MLX42/MLX42.h:
-src/../lib/libft/libft.h:
