8066ff2a5ff2a7da4bdfa9367a5b236e701e63ac
Kate <108870914+LeelooDa11as@users.noreply.github.com>
2025-03-07 14:22:20 +0100

    limpieza de obj

--- obj/r_render_images.d
+++
@@ -1,5 +0,0 @@
-obj/r_render_images.o: src/r_render_images.c src/cube3d.h \
-  src/../lib/MLX42/include/MLX42/MLX42.h src/../lib/libft/libft.h
-src/cube3d.h:
-src/../lib/MLX42/include/MLX42/MLX42.h:
-src/../lib/libft/libft.h:
