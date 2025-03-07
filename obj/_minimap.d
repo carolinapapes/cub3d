ba18f34a87f0d85c5fd825b0fe42afa3ae166bf2
carolinapapes <64616132+carolinapapes@users.noreply.github.com>
2025-03-07 13:13:51 +0100

    fix texture y axis

--- obj/_minimap.d
+++
@@ -1,5 +0,0 @@
-obj/_minimap.o: src/_minimap.c src/cube3d.h \
-  src/../lib/MLX42/include/MLX42/MLX42.h src/../lib/libft/libft.h
-src/cube3d.h:
-src/../lib/MLX42/include/MLX42/MLX42.h:
-src/../lib/libft/libft.h:
--- obj/r_map_checkers 2.d
+++
@@ -1,5 +0,0 @@
-obj/r_map_checkers.o: src/r_map_checkers.c src/cube3d.h \
-  src/../lib/MLX42/include/MLX42/MLX42.h src/../lib/libft/libft.h
-src/cube3d.h:
-src/../lib/MLX42/include/MLX42/MLX42.h:
-src/../lib/libft/libft.h:
