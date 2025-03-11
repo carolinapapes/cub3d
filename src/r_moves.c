b924f1b10e714b90d59064683a501cd98772d80e
carolinapapes <64616132+carolinapapes@users.noreply.github.com>
2025-03-11 12:53:08 +0100

    add ignore and fix moves collition checker

--- src/r_moves.c
+++ src/r_moves.c
@@ -6,7 +6,7 @@
 /*   By: capapes <capapes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 18:50:40 by capapes           #+#    #+#             */
-/*   Updated: 2025/03/10 12:31:52 by capapes          ###   ########.fr       */
+/*   Updated: 2025/03/11 12:51:08 by capapes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static int	hits_wall(t_vector pos, int axis)
 	return (0);
 }
 
-static int	collition_check(int axis, int dir)
+int	collition_check(int axis, int dir)
 {
 	int			moves;
 	t_vector	position;
@@ -44,14 +44,23 @@ static int	collition_check(int axis, int dir)
 	}
 	return (moves);
 }
-// t_vector	compute_delta(int key)
+// t_vector	compute_delta(int axis, int dir)
 // {
 // 	double		pov;
 // 	t_vector	delta;
+// 	int			key;
 
 // 	pov = get_player().pov;
 // 	delta.x = 0;
 // 	delta.y = 0;
+// 	if (axis == Y && dir == POSITIVE)
+// 		key = MLX_KEY_W;
+// 	if (axis == Y && dir == NEGATIVE)
+// 		key = MLX_KEY_S;
+// 	if (axis == X && dir == NEGATIVE)
+// 		key = MLX_KEY_A;
+// 	if (axis == X && dir == POSITIVE)
+// 		key = MLX_KEY_D;
 // 	if (key == MLX_KEY_W) // axis y - dir +
 // 	{
 // 		delta.x = cos(pov);
@@ -75,29 +84,34 @@ static int	collition_check(int axis, int dir)
 // 	return (delta);
 // }
 
-	// delta.arr[!axis] = 0;
-	// delta.arr[axis] = collition_check(axis, dir);
-	// delta = compute_delta(key);
-	// if (delta.arr[axis] != 0)
+// delta.arr[!axis] = 0;
+// delta.arr[axis] = collition_check(axis, dir);
+// delta = compute_delta(key);
+// if (delta.arr[axis] != 0)
+
+// TODO: ask kate about doubles and zero
 void	player_move(int axis, int dir)
 {
 	t_vector	delta;
 	double		pov;
-	t_vector	quadrant;
+    t_vector	quadrant;
 
 	pov = get_player().pov;
-	printf("pov: %f\n", pov);
 	delta.arr[axis] = sin(pov) * dir;
 	delta.arr[!axis] = cos(pov) * dir;
-	if (axis == X)
+	if (axis == X && delta.x)
 		delta.x *= -1;
-	quadrant.x = (delta.x > 0) - (delta.x < 0);
-	quadrant.y = (delta.y > 0) - (delta.y < 0);
-	delta.arr[axis] = collition_check(axis, quadrant.arr[axis]);
-	delta.arr[!axis] = collition_check(!axis, quadrant.arr[!axis]);
-	if ((delta.x != 0 && delta.y != 0)
-		|| (delta.x != 0 && !quadrant.y)
-		|| (delta.y != 0 && !quadrant.x))
+	quadrant.arr[axis] = ((delta.arr[axis] > 0) - (delta.arr[axis] < 0));
+	quadrant.arr[!axis] = (delta.arr[axis] > 0) - (delta.arr[!axis] < 0);
+	if (delta.arr[axis] > 0.1 || delta.arr[axis] < -0.1)
+		delta.arr[axis] = collition_check(axis, quadrant.arr[axis]);
+	if (delta.arr[!axis] > 0.1 || delta.arr[!axis] < -0.1)
+		delta.arr[!axis] = collition_check(!axis, quadrant.arr[!axis]);
+	if (delta.x != 0 && delta.y != 0)
+		set_player(delta, 0);
+	else if (delta.x != 0 && quadrant.y == 0)
+		set_player(delta, 0);
+	else if (delta.y != 0 && quadrant.x == 0)
 		set_player(delta, 0);
 }
 
