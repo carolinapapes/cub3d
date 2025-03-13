6e4517140a399ae09be76ea610eed2f86445cd92
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2025-03-13 16:27:53 +0100

    color working and parsing

--- src/parser/colour.c
+++ src/parser/colour.c
@@ -6,13 +6,124 @@
 /*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 18:35:14 by kkoval            #+#    #+#             */
-/*   Updated: 2025/03/12 20:58:46 by kkoval           ###   ########.fr       */
+/*   Updated: 2025/03/13 16:26:46 by kkoval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cube3d.h"
 
-int extract_color(char *pigment)
+int extract_pigment(char *pigment, unsigned char *chosen_pigment)
+{
+	int num;
+
+	num = ft_atoi(pigment);
+	if (num >= 0 && num <= 255)
+	{
+		*chosen_pigment = (unsigned char)num;
+		return (0);
+	}
+	return (1);
+}
+
+int check_pigment(char *line, int line_len, unsigned char *pigment)
+{
+	int		i;
+	int		num_len;
+	char	*new_pigment;
+
+	i = 0;
+	num_len = 0;
+	while (line[i] != '\0' && line[i] == ' ')
+		i++;
+	printf("entrado en check |%c|\n", line[i]);
+	if (line[i] == '-' || line[i] == '\0')
+		return (1);
+	if (line[i] == '+')
+		i++;
+	while (i < line_len)
+	{
+		if (line[i] == ' ')
+			break;
+		if (ft_isdigit(line[i]) == 0)
+			return (1);
+		i++;
+		num_len++;
+	}
+	if (num_len > 3)
+		return (1);
+	new_pigment = ft_substr(line, i - num_len, num_len);
+	if (!new_pigment)
+		return (1);
+	if (extract_pigment(new_pigment, pigment) == 1)
+	{
+		free(new_pigment);
+		return (1);
+	}
+	free(new_pigment);
+	return (0);
+}
+
+int color_extract(char *line, t_color *color)
+{
+	int i;
+	int start;
+
+	i = 0;
+	while (line[i] && line[i] != ',')
+		i++;
+	if (line[i] == '\0')
+		return (1);
+	printf("antes del primer pigmento|%c|\n", line[i]);
+	if (check_pigment(line, i, &color->r) == 1)
+		return (1);
+	start = ++i;
+	while (line[i] && line[i] != ',')
+		i++;
+	if (line[i] == '\0')
+		return (1);
+	if (check_pigment(line + start, i - start, &color->g) == 1)
+		return (1);
+	start = ++i;
+	while (line[i] && line[i] != ' ' && line[i] != '\0')
+		i++;
+	if (check_pigment(line + start, i - start, &color->b) == 1)
+		return (1);
+
+	color->a = 255;
+	color->rgba = (color->r << 24) | (color->g << 16) | (color->b << 8) | color->a;
+	color->repeated++;
+	return (0);
+}
+
+int parse_color_line(char *line, t_color *color)
+{
+	printf("%c\n", *line);
+	line++;
+	if (*line != ' ' || !*line)
+		return (1);  //ask for this case F89 no space between letter and number
+	printf("%c\n", *line);
+	while (*line && *line == ' ')
+		line++;
+	if (!*line)
+		return (1);
+	if (color_extract(line, color) == 1)
+		return (1);
+	return (0);
+}
+
+int is_color(char *line, t_start *start)
+{
+	int res = 1;
+	while (*line && *line == ' ')
+		line++;
+	if (ft_strncmp(line, "F", 1) == 0 && start->floor.repeated == -1)
+		res = parse_color_line(line, &(start->floor));
+	else if (ft_strncmp(line, "C", 1) == 0 && start->ceiling.repeated == -1)
+		res = parse_color_line(line, &(start->ceiling));
+	return (res);
+}
+
+/*int extract_color(char *pigment)
 {
 	int color;
 	int	i;
@@ -111,15 +222,66 @@ void fix_palet(char **palet)
     }
 }
 
+
+int extract_pigment(char *pigment, char *chosen_pigemnt)
+{
+	int	num;
+
+	num = ft_atoi(pigment);
+	if (num >= 0 && num <= 255)
+	{
+		(*chosen_pigemnt) = num;
+		return (0);
+	}
+	return (1);
+}
+
+int	check_pigment(char *line, int line_len, unsigned char pigment)
+{
+	int		i;
+	char	*new_pigment;
+
+	i = 0;
+	if (line[0] = '-')
+		return (1);
+	if (*line == '+')
+		line++;
+	while (i < line_len)
+	{
+		if (line[i] == ' ')
+			break;
+		if (ft_isdigit(line[i]) == 0)
+			return (1);
+		i++;
+	}
+	if (i > 3)
+		return (1);
+	new_pigment = ft_substr(line, 0, 3);
+	if (extract_pigment(new_pigment &pigment) == 1)
+	{
+		free(new_pigment);
+		return (1); // error
+	}
+	free (new_pigment);
+	return (0);
+}
+
 int	color_extract(char *line, t_color *color)
 {
 	int	i;
 
 	i = 0;
-	while (line[i] && line[i] != ',' && line[i] != ' ')
+	while (line[i] && line[i] != ',')
 		i++;
+	if (line[i] == '/0')
+		return (1);
+	if (line[i] !=  ',')
+		return (1);
+	if (check_pigment(line, i, color->r) == 1)
+		return (1);
 	
-	if (line[i] == ',' || line[i] != ' ')
+	// se hace la suma de todos pigmentos
+	
 	
 }
 
@@ -128,7 +290,6 @@ int	parse_color_line (char *line, t_color *color)
 	int	i;
 
 	i = 0;
-
 	while (*line && *line == ' ')
 		line++;
 	if (*line == '\0')
@@ -140,35 +301,38 @@ int	parse_color_line (char *line, t_color *color)
 	*line++;
 	if (*line == '\0')
 		return (1);
-	if (color_extract == 1)
+	if (color_extract(line, color->r) == 1)
 		return (1);
+	while (*line && line == ' ')
+		line++;
+	if (*line == '\0')
+		return (1);
+	if (color_extract(line, color->g) == 1)
+		return (1);
+	while (*line && line == ' ')
+		line++;
+	if (*line == '\0')
+		return (1);
+	if (color_extract(line, color->b) == 1)
+		return (1);
+	color->a = 255;
+	color->rgba = r g b a (bit shifted);
+	color->repeated++;
+	return (0);
 }
 
 int	is_color(char *line, t_start *start)
 {
-	char	**palet;
-	int		res;
-	int		i;
+	int	res;
+	int	i;
 
 	res = 1;
 	i = 0;
-	palet  = ft_split(line, ' ');
-	while(palet[i] != NULL)
-	{
-		//printf("%s\n", palet[i]); //Delete after better coma handeling
-		i++;
-	}
-	fix_palet(palet);
-	i = 0;
-	while(palet[i] != NULL)
-	{
-		//printf("|%s|\n", palet[i]); //Delete later
-		i++;
-	}
-	if (ft_strncmp(palet[0], "F", 1) == 0 && start->floor.repeated == -1)
-		res = check_line_for_color(palet, &(start->floor));
-	else if (ft_strncmp(palet[0], "C", 1) == 0 && start->ceiling.repeated == -1)
-		res = check_line_for_color(palet, &(*start).ceiling);
-	free_char_array(&palet);
+	while (line[i] != '/0' && *line == ' ')
+		line++;
+	if (ft_strncmp(line[i], "F", 1) == 0 && start->floor.repeated == -1)
+		res = check_line_for_color(line[i], &(start->floor));
+	else if (ft_strncmp(line[i], "C", 1) == 0 && start->ceiling.repeated == -1)
+		res = check_line_for_color(line[i], &(*start).ceiling);
 	return (res);
-}
+}*/
